72f427120c52565d458cf354bc65834f8e2d1d61
ray <egg0323@gmail.com>
2017-03-17 12:22:18 +0800

    專案建立 1. 更新bootstrap, jquery, json 2. 新聞頁面建立 3. Database建立

---
+++ HtmlPractice/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using HtmlPractice.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            HomePageEntities db = new HomePageEntities();
+            var viewModel = db.NewsInfo.OrderByDescending(x => x.CreateTime).ToList();
+
+            return PartialView("_NewsPanel", viewModel);
+        }
+
+        public ActionResult GetAsside()
+        {
+            HomePageEntities db = new HomePageEntities();
+            var viewModel = db.NewsInfo.OrderByDescending(x => x.CreateTime).ToList();
+
+            return PartialView("_NewsAssidePanel", viewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
